64762e8b52c6eed530d78d41e2686a08243f372a
samithspalan <samithpoojary12345@gmail.com>
2025-02-19 20:43:04 +0530

    first commit

--- script.js
+++ script.js
@@ -2,19 +2,43 @@ let prompt=document.querySelector(".prompt");
 let btn=document.querySelector(".btn");
 let chatContainer=document.querySelector(".chatContainer");
 let userMessage=null;
+let Api_url=;
 function createchat(html,classname){
     let div=document.createElement("div");
     div.classList.add(classname);
     div.innerHTML=html;
     return div;
 }
+async function  getapiResponse(aibox) {
+    try{
 
+    }catch{
+
+    }
+    
+}
+function showloading(){
+    let html=`<div class="chatbot">
+            <div class="img">
+                <img src="chatbot.png" width="30">
+            </div>
+            
+            <p class="text"></p>
+            <img class="loading" src="load.gif" width="50">
+        </div>`;
+        let aibox= createchat(html,"chatbot"); 
+        // aibox.querySelector(".text").innerText;
+        chatContainer.appendChild(aibox);
+        getapiResponse(aibox);
+    
+
+}
 btn.addEventListener('click',()=>{
     userMessage=prompt.value;
     if(!userMessage)return;
     let html=` <div class="user">
     <div class="img">
-            <img src="icons.png" width="50">
+            <img src="icons.png" width="30">
             </div>
             <p class="text"></p>
         </div>`
@@ -22,5 +46,5 @@ btn.addEventListener('click',()=>{
      userbox.querySelector(".text").innerText =userMessage;
      chatContainer.appendChild(userbox);
      prompt.value="";
-
+    setTimeout(showloading,500);
 })
