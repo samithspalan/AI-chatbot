3ced0eacb1cc5ec5626d790f50bb3874342a77cf
samithspalan <samithpoojary12345@gmail.com>
2025-02-19 23:17:50 +0530

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let prompt=document.querySelector(".prompt");
+let container=document.querySelector(".container");
 let btn=document.querySelector(".btn");
 let chatContainer=document.querySelector(".chatContainer");
 let userMessage=null;
 let Api_url=;
-function createchat(html,classname){
-    let div=document.createElement("div");
-    div.classList.add(classname);
-    div.innerHTML=html;
+
+function createchat(html, classname) {
+    let div = document.createElement("div");
+    div.className = classname; 
+    div.innerHTML = html.trim();
     return div;
 }
+
 async function  getapiResponse(aibox) {
      let text=aibox.querySelector(".text");
     try{
@@ -34,33 +37,53 @@ async function  getapiResponse(aibox) {
     }
     
 }
-function showloading(){
-    let html=`<div class="chatbot">
-            <div class="img">
-                <img src="chatbot.png" width="30">
-            </div>
-            
-            <p class="text"></p>
-            <img class="loading" src="load.gif" width="50">
-        </div>`;
-        let aibox= createchat(html,"chatbot"); 
-        chatContainer.appendChild(aibox);
-        getapiResponse(aibox);
-    
+function showloading() {
+    let chatbotHtml = `
+        <div class="img">
+            <img src="chatbot.png" width="30">
+        </div>
+        <p class="text"></p>
+        <img class="loading" src="load.gif" width="50">
+    `;
 
+    let aibox = createchat(chatbotHtml, "chatbot");
+    chatContainer.appendChild(aibox);
+    
+    getapiResponse(aibox);
 }
-btn.addEventListener('click',()=>{
-    userMessage=prompt.value;
-    if(!userMessage)return;
-    let html=` <div class="user">
-    <div class="img">
-            <img src="icons.png" width="30">
-            </div>
-            <p class="text"></p>
-        </div>`
-     let userbox= createchat(html,"user");  
-     userbox.querySelector(".text").innerText =userMessage;
-     chatContainer.appendChild(userbox);
-     prompt.value="";
-    setTimeout(showloading,500);
-})
+
+// btn.addEventListener('click',()=>{
+//     console.log("button");
+//     if(userMessage==""){
+//         container.style.display="flex";
+//     }else {
+//         container.style.display="none";
+
+//     }
+//     userMessage=prompt.value;
+//     if(!userMessage)return;
+//     let html=` <div class="user">
+//     <div class="img">
+//             <img src="icons.png" width="30">
+//             </div>
+//             <p class="text"></p>
+//         </div>`
+//      let userbox= createchat(html,"user");  
+//      userbox.querySelector(".text").innerText =userMessage;
+//      chatContainer.appendChild(userbox);
+//      prompt.value="";
+//     setTimeout(showloading,500);
+// })
+btn.addEventListener("click", () => {
+    userMessage = prompt.value.trim();
+     container.style.display = "none"; 
+
+    let userHtml = `<div class="img">
+                        <img src="icons.png" width="30">
+                    </div>
+                    <p class="text">${userMessage}</p>`;
+    let userbox = createchat(userHtml, "user");
+    chatContainer.appendChild(userbox);
+    prompt.value = ""; 
+    setTimeout(showloading, 500);
+});
